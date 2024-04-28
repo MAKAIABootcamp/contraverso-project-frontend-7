4af438474f59358f057295a3c07627592957cc06
Gabriela Aguiar <gabrielaaguiarmarquez@gmail.com>
2024-04-28 13:07:01 -0500

    Importar fuentes.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
 import "./styles/universal.scss";
+import "../fonts/fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
