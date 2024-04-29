b440e4f47d98789fca8e702fa9d619aacaa7449e
Juliana Argaez <argaez8533025@gmail.com>
2024-04-29 13:40:04 -0500

    Responsive movil para unirme y links en los botones

--- src/components/Inicio/InicioUnirme.jsx
+++ src/components/Inicio/InicioUnirme.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import "../../../fonts/fonts.css";
+import { NavLink } from "react-router-dom";
 
 const StyledInicioUnirme = styled.div`
   background-image: url("https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336886/MONTAJE/INICIO/2_duvvti.png");
@@ -11,79 +12,113 @@ const StyledInicioUnirme = styled.div`
   justify-content: space-between;
 
   @media screen and (max-width: 480px) {
-    flex-direction: column-reverse;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
+    width: 100%;
   }
-
 `;
 
 const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   width: 60%;
-  padding-right: 5%;
-  
-`;
-const StyledContainerText = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
-const StyledButtonUnirme = styled.button`
-  background-color: #1df4c8;
-  color: #000000;
-  padding: 1rem;
-  font-family: "MADE Soulmaze";
-  border: none;
-  border-radius: 8px;
-  align-self: center;
-`;
-
-const StyledImgIcono = styled.img`
-width: 40%;
-height: auto;
-`;
-
-const StyledImgLogo = styled.img`
-
-align-self: flex-end;
+  @media screen and (max-width: 480px) {
+    width: 100%;
+    padding-left:5%;
+  }
 `;
 
 const StyledTexto = styled.p`
-  color: #f1f1d8;
-  background-color: #000000;
+  margin-top: 8rem;
+  @media screen and (max-width: 480px) {
+    margin-top: 0;
+   text-align: center;
   
-  font-size: x-large;
-
-margin-bottom: 4%;
-  
+  }
 `;
 
+const StyledSpan = styled.span`
+  color: #f1f1d8;
+  background-color: #000000;
+  font-family: "Filson Pro Book";
+  font-size: 3vw;
+  line-height: 1.5;
+  margin-bottom: 4%;
+
+  @media screen and (max-width: 480px) {
+    font-size: 5vw;
+  }
+`;
+
+const StyledButtonUnirme = styled(NavLink)`
+  background-color: #1df4c8;
+  color: #000000;
+  padding: 0.8rem;
+  font-family: "MADE Soulmaze";
+  border: none;
+  border-radius: 8px;
+  align-self: flex-start;
+  margin-top: 1rem;
+  margin-left: 30%;
+  text-decoration: none;
+
+  @media screen and (max-width: 480px) {
+    margin-left: 0;
+    align-self: center;
+  }
+
+`;
+
+const StyledImgIcono = styled.img`
+  width: 40%;
+  height: auto;
+
+  @media screen and (max-width: 480px) {
+    width: 60%;
+  }
+
+`;
+
+const StyledImgLogo = styled.img`
+width: 36%;
+  align-self: flex-end;
+  margin-bottom: 2%;
+  margin-right: 4%;
+
+  @media screen and (max-width: 480px) {
+    width: 30%;
+    justify-content: flex-end;
+    margin-top: 5%;
+  }
+`;
 
 const InicioUnirme = () => {
-    return (
-        <StyledInicioUnirme>
-            <StyledImgIcono
-                src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336882/MONTAJE/INICIO/1-2_on2umt.png"
-                alt="icono"
-            ></StyledImgIcono>
+  return (
+    <StyledInicioUnirme>
+      <StyledImgIcono
+        src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336882/MONTAJE/INICIO/1-2_on2umt.png"
+        alt="icono"
+      ></StyledImgIcono>
 
-            <StyledContainer>
-                <StyledContainerText>
-                    <StyledTexto>
-                        ¡Aprende, exprésate y ayúdanos a
-                    </StyledTexto>
-                    <StyledTexto>
-                        combatir la desinformación!
-
-                    </StyledTexto>
-                    <StyledButtonUnirme>Unirme</StyledButtonUnirme>
-                </StyledContainerText>
-                <StyledImgLogo src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336881/MONTAJE/INICIO/1-7-2_lbpqjx.png" alt="logo"></StyledImgLogo>
-            </StyledContainer>
-        </StyledInicioUnirme>
-    );
+      <StyledContainer>
+        <StyledTexto>
+          <StyledSpan>
+            ¡Aprende, exprésate y ayúdanos a combatir la desinformación!
+          </StyledSpan>
+        </StyledTexto>
+        <StyledButtonUnirme to="https://discord.gg/tCqfzHWGCV">
+          Unirme
+        </StyledButtonUnirme>
+        <StyledImgLogo
+          src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336881/MONTAJE/INICIO/1-7-2_lbpqjx.png"
+          alt="logo"
+        ></StyledImgLogo>
+      </StyledContainer>
+    </StyledInicioUnirme>
+  );
 };
 
 export default InicioUnirme;
