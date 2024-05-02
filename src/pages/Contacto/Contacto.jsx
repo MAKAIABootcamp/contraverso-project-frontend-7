ebfa0648f10f7f67d33400bd687b1afc0475f06d
Estefania villan <evargasvi@cesde.net>
2024-05-02 15:42:54 -0500

    Creacion del componente QueBuscamos

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,11 +1,13 @@
 import "./Contacto.scss";
 import Contactanos from "../../components/Contactanos/Somos";
+import QueBuscamos from "../../components/Contactanos/QueBuscamos";
 
 const Contacto = () => {
   return (
       <>
         <div className="contacto">Contacto</div>
         <Contactanos/>
+        <QueBuscamos/>
       </>
   )
 };
