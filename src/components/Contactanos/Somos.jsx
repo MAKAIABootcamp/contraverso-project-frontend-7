c7d89d0c15cbf22e11016567fbf16299284b189f
Juliana Argaez <argaez8533025@gmail.com>
2024-05-07 19:25:13 -0500

    Responsive del somos y corrección de errores de estilos

--- src/components/Contactanos/Somos.jsx
+++ src/components/Contactanos/Somos.jsx
@@ -3,207 +3,93 @@ import "../../../fonts/fonts.css";
 
 const StyledSomos = styled.div`
   background-image: url("https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336904/MONTAJE/SOMOS/22_bajxgg.png");
-  height: 30rem;
+  height: 83vh;
   scroll-snap-align: start;
-`;
-const StyledTitulo = styled.h1`
-  font-family: "MADE Soulmaze";
-  color: #ffffff;
-  margin-top: 5rem;
-  position: absolute;
-  margin-left: 39%;
-  font-size: 7rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 
-  @media (min-width: 1024px) and (max-width: 1366px) {
-    margin-left: 46%;
-    font-size: 4rem;
-    margin-top: 2%;
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+    justify-content: center;
   }
-  @media (max-width: 1024px) {
-    margin-left: 46%;
-    font-size: 4rem;
-    margin-top: 2%;
+
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  height: 100%;
+  justify-content: center;
+
+  @media screen and (max-width: 500px) {
+    height: 50%;
+    width: 100%;
   }
-  @media (max-width: 768px) {
-    margin-left: 19rem;
-    font-size: 4rem;
+`;
+
+const StyledTitulo = styled.h1`
+  font-family: "MADE Soulmaze brush";
+  color: #ffffff;
+  font-size: 10em;
+
+  @media (max-width:1300px){
+    font-size: 8em;
   }
-  @media (max-width: 600px) {
-    margin-left: 12rem;
-    font-size: 4rem;
-  }
-  @media (max-width: 414px) {
-    margin-left: 7rem;
-    font-size: 3rem;
-  }
-  @media (max-width: 375px) {
-    margin-left: 5rem;
-  }
-  @media (max-width: 320px) {
-    margin-left: 3rem;
-    font-size: 3rem;
-    margin-block: auto;
+
+  @media screen and (max-width: 700px) {
+    font-size: 3em;
+    align-self: center;
   }
 `;
 const StyledParrafo = styled.p`
   color: #ffffff;
-  font-family: "Filson Pro";
+  font-family: "Filson Pro Book";
   display: flex;
-  width: 38rem;
-  margin-top: 12rem;
-  position: absolute;
-  margin-left: 39%;
-  font-size: 1.8rem;
+  width: 100%;
+  font-size: 2em;
+  padding-left: 5%;
+  padding-right: 5%;
 
-  @media (min-width: 1024px) and (max-width: 1366px) {
-    margin-left: 46%;
-    font-size: 160%;
-    margin-top: 13%;
-    width: 44%;
+  @media (max-width:1300px){
+    font-size: 1.8em;
   }
-  @media (max-width: 1024px) {
-    margin-left: 46%;
-    font-size: 160%;
-    margin-top: 13%;
-    width: 44%;
-  }
-  @media (max-width: 768px) {
-    margin-left: 42%;
-    font-size: 173%;
-    margin-top: 13%;
-    width: 51%;
-  }
-  @media (max-width: 600px) {
-    margin-left: 15%;
-    font-size: 142%;
-    margin-top: 17%;
-    width: 76%;
-  }
-  @media (max-width: 414px) {
-    margin-left: 2%;
-    font-size: 126%;
-    margin-top: 23%;
-    width: 93%;
-    text-align: center;
-  }
-  @media (max-width: 375px) {
-    margin-left: 2%;
-    font-size: 126%;
-    margin-top: 23%;
-    width: 93%;
-    text-align: center;
-  }
-  @media (max-width: 320px) {
-    margin-left: 2%;
-    font-size: 126%;
-    margin-top: 23%;
-    width: 93%;
-    text-align: center;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1em;
+    align-self: center;
   }
 `;
-const StyledImgDog = styled.img`
-  width: 35rem;
-  height: 30rem;
 
-  @media (min-width: 1024px) and (max-width: 1366px) {
-    width: 40%;
-    margin-top: 5.5%;
-    height: 84.5%;
-  }
-  @media (max-width: 1024px) {
-    width: 46%;
-    margin-top: 3.1%;
-    height: 93.5%;
-  }
-  @media (max-width: 768px) {
-    height: 21rem;
-    margin-top: 19.5%;
-    width: 46%;
-    margin-left: 0%;
-  }
-  @media (max-width: 600px) {
-    height: 47%;
-    margin-top: 44%;
-    width: 35%;
-    margin-left: 10%;
-  }
-  @media (max-width: 414px) {
-    height: 14rem;
-    margin-top: 64.5%;
-    width: 54%;
-    margin-left: 4%;
-  }
-  @media (max-width: 375px) {
-    margin-left: 3%;
-    height: 12rem;
-    margin-top: 81%;
-    width: 54%;
-  }
-  @media (max-width: 320px) {
-    height: 9rem;
-    margin-top: 111%;
+const StyledImgDog = styled.img`
+  width: 40%;
+  max-height: 100%;
+  align-self: flex-end;
+  
+  @media (max-width:1300px){
+    align-self: center;
   }
 `;
 const StyledLogo = styled.img`
-  margin-left: 46rem;
-  position: absolute;
-  margin-top: 25rem;
-
-  @media (min-width: 1024px) and (max-width: 1366px) {
-    margin-top: 23rem;
-    width: 18%;
-    height: 10%;
-    margin-left: 34rem;
-  }
-  @media (max-width: 1024px) {
-    margin-left: 6%;
-    width: 26%;
-    height: 7%;
-    margin-top: 37%;
-  }
-  @media (max-width: 768px) {
-    margin-left: 6%;
-    width: 32%;
-    height: 7%;
-    margin-top: 53%;
-  }
-  @media (max-width: 600px) {
-    margin-left: 6%;
-    width: 32%;
-    height: 7%;
-    margin-top: 69%;
-  }
-  @media (max-width: 414px) {
-    margin-left: 6%;
-    width: 32%;
-    height: 7%;
-    margin-top: 103%;
-  }
-  @media (max-width: 375px) {
-    margin-left: 6%;
-    width: 32%;
-    height: 7%;
-    margin-top: 111%;
-  }
-  @media (max-width: 320px) {
-    margin-left: 6%;
-    width: 32%;
-    height: 7%;
-    margin-top: 136%;
-  }
+width: 30%;
+align-self: flex-end;
+padding-right: 5%;
 `;
 const Somos = () => {
   return (
     <StyledSomos>
-      <StyledTitulo>SOMOS</StyledTitulo>
-      <StyledParrafo>
-        un proyecto de investigación creación que busca fomentar procesos de
-        comunicación críticos, creativos y participativos. Nuestro objetivo es
-        contrarrestar el impacto negativo de la desinformación y aportar a la
-        transformación de la realidad.
-      </StyledParrafo>
       <StyledImgDog src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336897/MONTAJE/SOMOS/6-1_h6k3un.png"></StyledImgDog>
-      <StyledLogo src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336892/MONTAJE/SOMOS/6-2_wcgkbl.png"></StyledLogo>
+      <StyledContainer>
+        <StyledTitulo>SOMOS</StyledTitulo>
+        <StyledParrafo>
+          un proyecto de investigación creación que busca fomentar procesos de
+          comunicación críticos, creativos y participativos. Nuestro objetivo es
+          contrarrestar el impacto negativo de la desinformación y aportar a la
+          transformación de la realidad.
+        </StyledParrafo>
+        <StyledLogo src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336892/MONTAJE/SOMOS/6-2_wcgkbl.png"></StyledLogo>
+      </StyledContainer>
     </StyledSomos>
   );
 };
--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+import "../../../fonts/fonts.css";
 import InicioAprende from "../../components/Inicio/InicioAprende";
 import InicioChequea from "../../components/Inicio/InicioChequea";
 import InicioConfronta from "../../components/Inicio/InicioConfronta";
@@ -5,21 +7,25 @@ import InicioExpresa from "../../components/Inicio/InicioExpresa";
 import InicioPodcast from "../../components/Inicio/InicioPodcast";
 import InicioSomos from "../../components/Inicio/InicioSomos";
 import InicioUnirme from "../../components/Inicio/InicioUnirme";
-import "./Inicio.scss";
+
+const StyledInicio = styled.div`
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  gap: 5vh;
+`;
 
 const Inicio = () => {
   return (
-      <>
-        <div className="inicio">
-        <InicioSomos/>
-        <InicioUnirme/>
-        <InicioChequea/>
-        <InicioConfronta />
-        <InicioExpresa />
-        <InicioAprende />
-        <InicioPodcast />
-        </div>
-      </>
+    <StyledInicio>
+      <InicioSomos />
+      <InicioUnirme />
+      <InicioChequea />
+      <InicioConfronta />
+      <InicioExpresa />
+      <InicioAprende />
+      <InicioPodcast />
+    </StyledInicio>
   );
 };
 
