0f4f770d94556bd3a12ecb5cb0bc342abcb154c0
Juliana Argaez <argaez8533025@gmail.com>
2024-05-10 22:49:53 -0500

    responsive Juegos vistas grande

--- src/components/AprendeComponents/AprendeJuega/AprendeJuega.jsx
+++ src/components/AprendeComponents/AprendeJuega/AprendeJuega.jsx
@@ -1,31 +1,41 @@
-import React from "react";
 import styled from "styled-components";
 import Button from "../../Button/Button";
 
 const SectionStyled = styled.section`
   background-image: url("https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336853/MONTAJE/APRENDE/21_waoapm.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
   width: 100%;
-  height: 100vh;
+  height: 95vh;
   position: relative;
   scroll-snap-align: start;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const ContainerStyled = styled.div`
-  position: absolute;
-  bottom: 63%;
-  left: 7%;
+  width: 40%;
+  height: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5%;
+  
+
   img {
-    width: auto;
-    height: 14vh;
+    position: relative;
+    width: 100%;
+    padding: 5%;
+    
   }
   h1 {
     font-size: 6rem;
     font-family: "MADE Soulmaze Brush";
     position: absolute;
-    bottom: -4%;
-    left: 23%;
   }
-  @media (max-width: 768px) {
+  /* @media (max-width: 768px) {
     bottom: 73%;
     left: 20%;
     img {
@@ -57,17 +67,27 @@ const ContainerStyled = styled.div`
     h1 {
       font-size: 3.5rem;
     }
-  }
+  } */
+`;
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 70%;
+  display: flex;
+  flex-direction: row;
+  position: relative;
 `;
 
 const ButtonsStyled = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  position: relative;
-  top: 49%;
+  position: absolute;
   gap: 1.2rem;
-  @media (max-width: 768px) {
+  width: 100%;
+  height: 100%;
+  /* @media (max-width: 768px) {
     top: 37%;
   }
   @media (max-width: 414px) {
@@ -78,37 +98,38 @@ const ButtonsStyled = styled.div`
   }
   @media (max-width: 320px) {
     top: 33%;
-  }
+  } */
 `;
 
 const PerroStyled = styled.img`
-  widht: auto;
-  height: 77vh;
-  position: absolute;
-  top: 23%;
-  right: 0;
+  width: 50%;
+  max-height: 100%;
   z-index: 1;
-  @media (max-width: 768px) {
+  align-self: flex-end;
+
+  /* @media (max-width: 768px) {
     top: 53%;
     height: 47vh;
-  }
+  } */
 `;
 
 const TextureStyled = styled.figure`
-  position: absolute;
-  transform: translate(0, 39%);
+  display: flex;
+  width: 50%;
+  padding-right: 10%;
+  max-height: 80%;
+  align-self: flex-end;
   img {
     opacity: 0.5;
-    width: auto;
-    height: 60vh;
+    width: 100%;
     z-index: 0;
   }
-  @media (max-width: 768px) {
+  /* @media (max-width: 768px) {
     img {
       height: 45vh;
     }
     transform: translate(0, 96.8%);
-  }
+  } */
 `;
 
 const AprendeJuega = () => {
@@ -125,16 +146,19 @@ const AprendeJuega = () => {
         <Button>JUEGOS EN LÍNEA</Button>
         <Button>JUEGOS DE MESA</Button>
       </ButtonsStyled>
-      <PerroStyled
-        src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336848/MONTAJE/APRENDE/5-2_pisnsy.png"
-        alt="Perro sentado en la mesa"
-      />
-      <TextureStyled>
-        <img
-          src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336837/MONTAJE/APRENDE/5-2_TEXTU_pnnfs8.png"
-          alt="Textura"
-        />
-      </TextureStyled>
+
+      <StyledContainer>
+        <TextureStyled>
+          <img
+            src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336837/MONTAJE/APRENDE/5-2_TEXTU_pnnfs8.png"
+            alt="Textura"
+          />
+        </TextureStyled>
+        <PerroStyled
+          src="https://res.cloudinary.com/dvafjaqbd/image/upload/v1714336848/MONTAJE/APRENDE/5-2_pisnsy.png"
+          alt="Perro sentado en la mesa"
+        ></PerroStyled>
+      </StyledContainer>
     </SectionStyled>
   );
 };
