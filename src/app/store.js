7e8ae556e978a3d77b7297dbe3f7231fe98e0260
Estefania villan <evargasvi@cesde.net>
2024-05-21 18:27:31 -0500

    Se crean botones y se resuelve conflicto

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import userAuthReducer from './features/userAuth/userAuthSlice'
 import filtersByButtonsReducer from './features/filtersByButtons/filtersByButtonsSlice';
 import imgsReducer from "./CarruselIMG/imgsSlice.js";
+import fanzineReducer from './CarruselFanzines/fanzinesSlices'
 
 const store = configureStore({
   reducer:  {
     userAuth: userAuthReducer,
     filtersByButtons: filtersByButtonsReducer,
     imgs: imgsReducer,
+    fanzines: fanzineReducer
   },
   devTools: process.env.NODE_ENV !== "production",
 });
